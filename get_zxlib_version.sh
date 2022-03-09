e0e85e08448e2993099b206e09773975bbffcec3
Fernando Theirs <fntheirs@gmail.com>
2022-03-09 16:29:51 +0100

    fix issues when using script in CI

--- get_zxlib_version.sh
+++ get_zxlib_version.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(dirname $(readlink -f $0))
-
 source <(\
     sed -r '{
         /^#define/!d
         /#define[ \t]*[^ \t]*$/d
         s/[^ \t]*[ \t]*([^ \t]*)[ \t]*(.*)/\1="\2"/
         /(""|\([^\)]*\))/d
-    }' <(gcc -E -dM $SCRIPT_DIR/include/zxversion.h)
+    }' <(gcc -E -dM include/zxversion.h)
 )
